bc6295a812a4ea2e28fbac4e97db85d51051eb1e
Catevika <dominique.bello@outlook.fr>
2024-12-31 16:36:09 +0100

    Update productsRouter to productsRoutes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,7 @@
 import express, { json, urlencoded } from 'express';
 import serverless from "serverless-http";
 import authRoutes from './routes/auth/index.js';
-import productsRouter from './routes/products/index.js';
+import productsRoutes from './routes/products/index.js';
 
 const port = 3000;
 
@@ -14,7 +14,7 @@ app.use(urlencoded({ extended: false }));
 //   res.send('Hello World 123');
 // });
 
-app.use('/products', productsRouter);
+app.use('/products', productsRoutes);
 app.use('/auth', authRoutes);
 
 if (process.env.NODE_ENV === "dev") {
