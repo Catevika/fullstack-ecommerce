1ea23cdc2d799739bc6f736cc8744829a2b61ca4
Catevika <dominique.bello@outlook.fr>
2025-01-12 17:36:10 +0100

    Add status Payment_failed

--- dashboard/app/dashboard/orders/[id]/StatusSelector.tsx
+++ dashboard/app/dashboard/orders/[id]/StatusSelector.tsx
@@ -4,7 +4,7 @@ import { updateOrderStatus } from '@/app/dashboard/orders/[id]/actions';
 import { ChevronDownIcon } from '@/components/ui/icon';
 import { Select, SelectBackdrop, SelectContent, SelectDragIndicator, SelectDragIndicatorWrapper, SelectIcon, SelectInput, SelectItem, SelectPortal, SelectTrigger } from '@/components/ui/select';
 
-const statuses = [{ label: 'New', value: 'New' }, { label: 'Paid', value: 'Paid' }, { label: 'Shipped', value: 'Shipped' }, { label: 'Delivered', value: 'Delivered' }];
+const statuses = [{ label: 'New', value: 'New' }, { label: 'Paid', value: 'Paid' }, { label: 'Payment_failed', value: 'Payment failed' }, { label: 'Shipped', value: 'Shipped' }, { label: 'Delivered', value: 'Delivered' }];
 export default function StatusSelector({ status, id }: { status: string; id: number; }) {
   return (
     <Select className='ml-auto w-48' defaultValue={status} onValueChange={(value) => updateOrderStatus(id, value)}>
