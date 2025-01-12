451bdc9f58c2b8cb16c910b46c365a3279eda78f
Catevika <dominique.bello@outlook.fr>
2025-01-12 17:12:59 +0100

    Fix the Stripe webhook in index

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,13 +4,13 @@ import authRoutes from './routes/auth/index.js';
 import ordersRoutes from './routes/orders/index.js';
 import productsRoutes from './routes/products/index.js';
 import stripeRoutes from './routes/stripe/index.js';
+import { webhook } from './routes/stripe/stripeController.js';
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use('/stripe/webhook', express.raw({ type: 'application/json' }));
-
+app.use('/stripe/webhook', express.raw({ type: 'application/json' }), webhook);
 
 app.use(json());
 app.use(urlencoded({ extended: false }));
