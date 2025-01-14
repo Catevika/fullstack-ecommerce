2badfe4d3b0c9c1f00e9524620074f33ce6e3357
Catevika <dominique.bello@outlook.fr>
2025-01-14 18:19:01 +0100

    Add a custom deleteProduct that update the product only

--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -52,6 +52,22 @@ export async function updateProduct(req: Request, res: Response) {
   }
 }
 
+export async function deleteProduct(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const updatedFields = req.cleanBody;
+    const [product] = await db.update(productsTable).set(updatedFields).where(eq(productsTable.id, Number(id))).returning();
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).send({ error: 'Product not found' });
+      return;
+    }
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+}
+
 // export async function deleteProduct(req: Request, res: Response) {
 //   const { id } = req.params;
 
