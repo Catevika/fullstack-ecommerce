6a2d16b00bd1ed24b085952483460eb3873f9854
Catevika <dominique.bello@outlook.fr>
2025-01-14 19:53:37 +0100

    Remove deleteProduct route as replaced by updatePRoduct

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { createProductSchema, updateProductSchema } from '../../db/schema/productsSchema.js';
 import { verifySeller, verifyToken } from '../../middlewares/authMiddleware.js';
 import { validateData } from '../../middlewares/validationMiddleware.js';
-import { createProduct, deleteProduct, getProductById, listProducts, updateProduct } from './productsController.js';
+import { createProduct, getProductById, listProducts, updateProduct } from './productsController.js';
 
 const router = Router();
 
@@ -10,6 +10,5 @@ router.get('/', listProducts);
 router.get('/:id', getProductById);
 router.post('/', verifyToken, verifySeller, validateData(createProductSchema), createProduct);
 router.put('/:id', verifyToken, verifySeller, validateData(updateProductSchema), updateProduct);
-router.delete('/:id', verifyToken, verifySeller, deleteProduct);
 
 export default router;
