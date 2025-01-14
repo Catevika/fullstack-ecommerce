daab8a7eed1007f40b46af4be24b6edbf54c81f2
Catevika <dominique.bello@outlook.fr>
2025-01-14 19:56:15 +0100

    Remove unused Dashboard from Sidebar & MobileNavBar + redirect Home to dashboard/products

--- dashboard/app/dashboard/layout.tsx
+++ dashboard/app/dashboard/layout.tsx
@@ -5,9 +5,10 @@ import { Heading } from '@/components/ui/heading';
 import { HStack } from '@/components/ui/hstack';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
-import { House, List, ShoppingBag } from 'lucide-react';
+import { List, ShoppingBag } from 'lucide-react';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+
 export default async function DashboardLayout({ children }: { children: React.ReactNode; }) {
   // For production, add a validation of the token and a regenerate if expired...
   const token = await getToken();
@@ -40,12 +41,6 @@ function Header() {
 function SideBar() {
   return (
     <VStack className='bg-gray-50 p-3 border-r-2 border-slate-300 hidden md:flex'>
-      <Link href={'/dashboard'}>
-        <HStack className='w-full mb-4'>
-          <House color="black" size={24} />
-          <Text className='ml-2'>Dashboard</Text>
-        </HStack>
-      </Link>
       <Link href={'/dashboard/products'}>
         <HStack className='w-full mb-4'>
           <ShoppingBag color="black" size={24} />
@@ -65,11 +60,6 @@ function SideBar() {
 function MobileNavbar() {
   return (
     <HStack className='bg-gray-50 pt-6 border-t-2 border-slate-300 shadow-md shadow-slate-300 flex flex-1 justify-around items-center absolute bottom-0 left-0 right-0 md:hidden'>
-      <Link href={'/dashboard'}>
-        <HStack className='w-full mb-4'>
-          <House color="black" size={24} />
-        </HStack>
-      </Link>
       <Link href={'/dashboard/products'}>
         <HStack className='w-full mb-4'>
           <ShoppingBag color="black" size={24} />
